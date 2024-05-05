7b4300ffb1a2814b0327471d1bda4855d1faa004
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2024-05-05 19:34:01 +0200

    Create login model

--- lib/features/login_screen/data/models/login_model.dart
+++ lib/features/login_screen/data/models/login_model.dart
@@ -1,12 +1,12 @@
-class LoginMoel {
+class LoginModel {
   int? id;
   String? fullName;
   String? email;
   String? token;
 
-  LoginMoel({this.id, this.fullName, this.email, this.token});
+  LoginModel({this.id, this.fullName, this.email, this.token});
 
-  LoginMoel.fromJson(Map<String, dynamic> json) {
+  LoginModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     fullName = json['fullName'];
     email = json['email'];
