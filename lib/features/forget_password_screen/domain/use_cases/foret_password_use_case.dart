a3ae44c84288d9b0bda8d3309ec7d0cd89975f3a
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2024-05-05 23:03:52 +0200

    Create ForgetPasswordUseCase class

---
+++ lib/features/forget_password_screen/domain/use_cases/foret_password_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../data/models/forget_password_model.dart';
+import '../repositories/forget_password_doman_repo.dart';
+
+class ForgetPasswordUseCase {
+
+  ForgetPasswordUseCase({required this.forgetPasswordDomainRepo});
+
+  ForgetPasswordDomainRepo forgetPasswordDomainRepo;
+
+  Future<Either<FailureError, ForgetPasswordModel>> forgetPassword()  {
+    return forgetPasswordDomainRepo.forgetPassword();
+  }
+}
