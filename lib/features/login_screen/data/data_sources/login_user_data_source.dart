3357fbf5b16ee577b0c2d1e1522280cbde4049d0
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2024-05-05 20:45:29 +0200

    Create remote & local loginUser dara source

--- lib/features/login_screen/data/data_sources/login_user_data_source.dart
+++ lib/features/login_screen/data/data_sources/login_user_data_source.dart
@@ -1,8 +1,42 @@
-import 'package:dartz/dartz.dart';
+import 'dart:developer';
 
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../../../../core/api/end_points.dart';
 import '../../../../core/error/failures.dart';
 import '../models/login_model.dart';
 
 abstract class LoginUserDataSource {
   Future<Either<FailureError, LoginModel>> loginUser(String email, String password);
+}
+
+class RemoteLoginUserDataSource extends LoginUserDataSource {
+  @override
+  Future<Either<FailureError, LoginModel>> loginUser(String email, String password) async {
+    final dio = Dio();
+    final response = await dio.post(
+      EndPoints.baseUrl + EndPoints.login,
+      data: {
+        "email": email,
+        "password": password
+      },
+    );
+    if (response.statusCode == 200) {
+
+      LoginModel loginModel = LoginModel.fromJson(response.data);
+      log(loginModel.toString());
+
+      return Right(loginModel);
+    } else {
+      return Left(FailureError(response.statusCode.toString()));
+    }
+  }
+}
+class LocalLoginUserDataSource extends LoginUserDataSource {
+  @override
+  Future<Either<FailureError, LoginModel>> loginUser(String email, String password) {
+    // TODO: implement loginUser
+    throw UnimplementedError();
+  }
 }
