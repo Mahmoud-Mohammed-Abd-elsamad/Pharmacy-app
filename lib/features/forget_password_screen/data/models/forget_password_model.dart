2dc1e417a1f98d70fc1e69ef7397dec8dd50d635
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2024-05-05 22:59:31 +0200

    Create forget password model

---
+++ lib/features/forget_password_screen/data/models/forget_password_model.dart
@@ -0,0 +1,11 @@
+class ForgetPasswordModel {
+  String? message;
+
+  ForgetPasswordModel({this.message});
+
+  ForgetPasswordModel.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+  }
+
+
+}
